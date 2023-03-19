a0bf9c36a9109cf2e06874ba6c97c0ff30c8c62f
2382546457 <2382546457@qq.com>
2023-03-19 09:38:13 +0800

    冒泡排序

---
+++ leetcode/src/各种排序/冒泡排序.java
@@ -0,0 +1,42 @@
+package 各种排序;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @author : 小何
+ * @Description :
+ * @date : 2023-03-19 09:24
+ */
+public class 冒泡排序 {
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length == 1)
+            return;
+
+        for (int i = 0; i < arr.length; i++) {
+            int flag = 0;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    flag = 1;
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+            if (flag == 0) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+        Random random = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            arr[i] = random.nextInt(1000);
+        }
+        bubbleSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
