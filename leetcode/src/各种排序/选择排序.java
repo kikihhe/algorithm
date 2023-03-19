ce41f76fb9a10bea066028e2a6dd24026ad91fca
2382546457 <2382546457@qq.com>
2023-03-19 09:44:36 +0800

    选择排序

---
+++ leetcode/src/各种排序/选择排序.java
@@ -0,0 +1,47 @@
+package 各种排序;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @author : 小何
+ * @Description :
+ * @date : 2023-03-19 09:39
+ */
+public class 选择排序 {
+    public static void selectSort(int[] arr) {
+        if (arr == null || arr.length == 1) {
+            return ;
+        }
+        for (int i = 0; i < arr.length; ) {
+            int max = arr[i];
+            int maxIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                    maxIndex = j;
+                }
+            }
+            // 交换
+            if (maxIndex!=i) {
+                int temp = arr[i];
+                arr[i] = arr[maxIndex];
+                arr[maxIndex] = arr[i];
+            }
+            i++;
+
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+        Random random = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            arr[i] = random.nextInt(10000);
+        }
+        selectSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
