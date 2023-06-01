ff27a3633c57463e82cbf5531de991952650c90f
2382546457 <2382546457@qq.com>
2023-06-01 16:44:15 +0800

    单子

--- leetcode/src/排序/快速排序/Main.java
+++ leetcode/src/排序/快速排序/Main.java
@@ -17,14 +17,19 @@ public class Main {
 
     }
     public static void quickSort(int[] arr, int low, int high) {
-        if (low >= high) return;
+        if (low >= high) {
+            return;
+        }
         // 获取中间位置，中间排序好之后排序它的左边和右边
         int mid = partition(arr, low, high);
+
         quickSort(arr, low, mid - 1);
         quickSort(arr, mid + 1, high);
     }
 
-    // 以arr[high]为基准，将小于它的数放到它左边，将大于它的数放到右边，最后返回它应该在的下标
+    /**
+     * 以arr[high]为基准，将小于它的数放到它左边，将大于它的数放到右边，最后返回它应该在的下标
+     */
     private static int partition(int[] arr, int low, int high) {
         int i = low;
         int p = arr[high];
