b15683cd6ef6e738ed9f8697c57b86a7f48bc4d1
2382546457 <2382546457@qq.com>
2023-06-21 18:56:31 +0800

    整数反转

--- leetcode/src/整数反转/Solution.java
+++ leetcode/src/整数反转/Solution.java
@@ -3,43 +3,51 @@ package 整数反转;
 import java.util.Scanner;
 
 class Solution {
-    public int reverse(int x) {
-        char[] arr = String.valueOf(x).toCharArray();
-        String str = func(arr);
-
-        if (str.compareTo(Integer.MAX_VALUE +"") > 0 || str.charAt(0) == '-' && str.substring(1, str.length()).compareTo(Integer.MAX_VALUE+"") > 0) {
-            return 0;
+    // 比较两个字符串对应的数值类型
+    private boolean compareTo(String str1, String str2) {
+        if (str1.charAt(0) != '-' && str2.charAt(0) != '-') {
+            if (str1.length() != str2.length()) {
+                return str1.length() > str2.length();
+            } else {
+                return str1.compareTo(str2) > 0;
+            }
+        } else {
+            str1 = str1.substring(1, str1.length());
+            str2 = str2.substring(1, str2.length());
+            if (str1.length() != str2.length()) {
+                return str1.length() > str2.length();
+            } else {
+                return str1.compareTo(str2) > 0;
+            }
         }
 
-
-        return Integer.parseInt(str);
     }
-    private String func(char[] arr) {
-        // - 4 2 2
-        int i = 0;
-        if (arr[0] == '-' || arr[0] == '+') {
-            i++;
+    public int reverse(int x) {
+        if (x == Integer.MIN_VALUE || x == 0) return 0;
+        String str = x + "";
+        if (x < 0) {
+            str = str.substring(1, str.length());
         }
-        int j = arr.length - 1;
-        while(j >= i && arr[j] == '0') {
-            j--;
-        }
-        String str = "";
-        if (arr[0] == '-' || arr[0] == '+') {
-            str += arr[0];
-        }
-        while (j >= i) {
-            str += arr[j];
-            j--;
+        StringBuilder s = new StringBuilder(str);
+        String ss = s.reverse().toString();
+
+
+        if (x < 0) ss = "-" + ss;
+
+        if (x > 0 && compareTo(ss, Integer.MAX_VALUE+"")) {
+            return 0;
+        } else if (x < 0 && compareTo(ss, (Integer.MIN_VALUE)+"")) {
+            return 0;
+        } else {
+            return Integer.valueOf(ss);
         }
 
-        return str;
 
     }
 
     public static void main(String[] args) {
-        Solution s = new Solution();
-        int reverse = s.reverse(123);
+        Solution solution = new Solution();
+        int reverse = solution.reverse(-2143847412);
         System.out.println(reverse);
     }
 }
