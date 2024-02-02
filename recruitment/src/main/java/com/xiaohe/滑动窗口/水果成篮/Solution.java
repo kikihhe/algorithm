7ab36c8d74c5f65663a049c223998557bd5fe009
2382546457 <2382546457@qq.com>
2024-02-02 18:33:55 +0800

    水果成篮（不会写）

---
+++ recruitment/src/main/java/com/xiaohe/滑动窗口/水果成篮/Solution.java
@@ -0,0 +1,18 @@
+package com.xiaohe.滑动窗口.水果成篮;
+
+
+/**
+ * 找出一个最长子数组，要求子数组中只能有两种数值，
+ */
+
+public class Solution {
+    public int totalFruit(int[] arr) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {3,3,3,1,2,1,1,2,3,3,4};
+        System.out.println(new Solution().totalFruit(arr));
+    }
+}
