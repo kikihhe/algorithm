c3fd5eb891cb75e00dfd9c735c65098dbbf80b89
heweifei <2382546457@qq.com>
2024-08-31 14:25:06 +0800

    合并两个有序链表

---
+++ autumn/src/main/java/com/xiaohe/链表/合并两个有序链表/Solution.java
@@ -0,0 +1,29 @@
+package com.xiaohe.链表.合并两个有序链表;
+
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        // 将两条链表看成两部分:
+        // 某链表中较小的节点
+        // 其他所有节点
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
